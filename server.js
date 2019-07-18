65bd58063bebbfb0e75f6e37f55490b92d88bc5c
Juan Manuel Molina <jmmolina79@gmail.com>
2019-07-18 14:49:43 -0500

    Segundo push

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.send('Servidor Iniciado')
+});
+
+
+app.get('/estudiantes', function(req, res) {
+    res.send('Hay 23 estudiantes')
+});
+
+app.get('/tutor', function(req, res) {
+    res.send('En clase hay 1 tutor')
+});
+
+app.listen(3000, function(){
+    console.log('servidor corriendo en el puerto 3000')
+})
