c06f67ee805d23070b33de4f4b4303867ce78d54
Juan Manuel Molina <jmmolina79@gmail.com>
2019-07-19 09:45:45 -0500

    controlador y clase transaciones

---
+++ app/controladores/transaccionControlador.js
@@ -0,0 +1,29 @@
+let Transaccion = require('../modelos/transaccion.js');//IMportamos la clase modelo
+//Creamos la clase controladora para manjar la informacion de los Transaccions
+class TransaccionControlador {
+    constructor() {   
+    }
+    //Funcion encargada de manejar la consulta de un Transaccion por id
+    consultaTransaccion(req, res) {
+        let id = req.params.id;
+        Transaccion.consultarTransaccion(id, (err, data) => {
+                if(data){
+                    res.json(data);
+                }else{
+                    res.send(err);
+                }
+            })
+    }
+    // Funcion encargada de manejar al consulta de todos los Transaccions de la base de datos
+    consultaTransacciones(req, res) {
+        Transaccion.consultaTransacciones((err, data) => {
+                if(data){
+                    res.json(data);
+                }else{
+                    res.send(err);
+                }
+            })
+    }  
+}
+const instanciaControlador = new TransaccionControlador();
+module.exports  = instanciaControlador;
