07036170e88ef84b23462c1c84dbdd76965a7838
Juan Manuel Molina <jmmolina79@gmail.com>
2019-07-19 09:53:40 -0500

    transacciones

--- app/controladores/transaccionControlador.js
+++ app/controladores/transaccionControlador.js
@@ -7,22 +7,22 @@ class TransaccionControlador {
     consultaTransaccion(req, res) {
         let id = req.params.id;
         Transaccion.consultarTransaccion(id, (err, data) => {
-                if(data){
-                    res.json(data);
-                }else{
-                    res.send(err);
-                }
-            })
+            if(data){
+                res.json(data);
+            }else{
+                res.send(err);
+            }
+        })
     }
     // Funcion encargada de manejar al consulta de todos los Transaccions de la base de datos
     consultaTransacciones(req, res) {
         Transaccion.consultaTransacciones((err, data) => {
-                if(data){
-                    res.json(data);
-                }else{
-                    res.send(err);
-                }
-            })
+            if(data){
+                res.json(data);
+            }else{
+                res.send(err);
+            }
+        })
     }  
 }
 const instanciaControlador = new TransaccionControlador();
