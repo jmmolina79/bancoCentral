65b51291d8debfce1281f10951eb0d83a9f30450
Juan Manuel Molina <jmmolina79@gmail.com>
2019-07-26 08:37:53 -0500

    cors

--- server.js
+++ server.js
@@ -5,9 +5,25 @@ const app = express();
 //Uso de Body Parser Para manejo de la informacion que viaja por las peticiones http al servidor
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+
+app.use(function(req, res, next) {
+    //res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "DELETE,POST,GET,OPTIONS,PUT,PATCH");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Credentials","true" )
+    res.header('Access-Control-Max-Age: 0'); 
+    next();
+});
+
+
+
 //Importamos la libreria o modulo router mandandole como parametro la instancia de servidor express
 require('./api/router.js')(app);
 //Iniciamos el servidor escuchando por un puerto  
 app.listen(9000, function(){
     console.log('EL servidor esta corriendo por el puerto 9000');
 });
+
